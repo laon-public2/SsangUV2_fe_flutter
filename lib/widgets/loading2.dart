7d83a2f86a426843c79fd779e6f1015f1a8bc51b
DAE HYEON HONG <hong4383@naver.com>
2021-04-16 15:48:19 +0900

    로딩 개선

---
+++ lib/widgets/loading2.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Loading2 extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading2> {
+  final List<String> randomCategory = [
+    "assets/icon/category/household.png",
+    "assets/icon/category/trip.png",
+    "assets/icon/category/sport.png",
+    "assets/icon/category/parenting.png",
+    "assets/icon/category/pet.png",
+    "assets/icon/category/laundry.png",
+    "assets/icon/category/shopping.png",
+    "assets/icon/category/furniture.png",
+    "assets/icon/category/car.png",
+    "assets/icon/category/etc.png"
+  ];
+
+  int num1 = 0;
+  bool _isDispose = false;
+  _randomNum() async {
+    bool infinite = false;
+    Random random = Random();
+    while (infinite == false) {
+      await Future.delayed(Duration(milliseconds: 200), () {
+        if(_isDispose == false){
+          setState(() {
+            num1 = random.nextInt(9);
+          });
+        }
+      });
+    }
+  }
+
+  void initState() {
+    super.initState();
+    _randomNum();
+  }
+
+  void dispose() {
+    super.dispose();
+    _isDispose = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _willPopCallback(),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Center(
+          child: Image.asset(randomCategory[num1], width: 48.0, height: 48.0,),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _willPopCallback() async {
+    return true;
+  }
+}
